f660b872ce3634fdf6051422def473af450c3c7e
SageblattN <s2931713@gmail.com>
2021-11-19 10:59:51 +0300

    Using groupby instead of loops fix

--- Pandas/second_part/second.py
+++ Pandas/second_part/second.py
@@ -1,24 +1,25 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("flights.csv")
 
-names = df['CARGO'].unique()
+names = np.sort(df['CARGO'].unique())
 
 fig, axes = plt.subplots(ncols=3, nrows=1, gridspec_kw={"wspace": 0.8}, figsize=[11, 8])
 
 data = [[], [], []]
-for p in names:
-    data[0].append([df[df["CARGO"] == p].shape[0], p])
-    data[1].append([int(df[df["CARGO"] == p]['PRICE'].sum()), p])
-    data[2].append([int(df[df["CARGO"] == p]['WEIGHT'].sum()), p])
+
+data[0] = df.groupby('CARGO').count()['Unnamed: 0'].to_list()
+data[1] = df.groupby('CARGO').sum()['PRICE'].to_list()
+data[2] = df.groupby('CARGO').sum()['WEIGHT'].to_list()
 
 del df
 
 for i in range(3):
-    axes[i].bar([0, 1, 2], height=[data[i][k][0] for k in range(3)])
+    axes[i].bar([0, 1, 2], height=[data[i][k] for k in range(3)])
     axes[i].set_xticks([0, 1, 2])
-    axes[i].set_xticklabels((k[1] for k in data[i]))
+    axes[i].set_xticklabels((p for p in names))
 
 axes[0].set_title("Flights amount")
 axes[1].set_title("Total price")
--- Pandas/third_part/third.py
+++ Pandas/third_part/third.py
@@ -1,35 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df1 = pd.read_excel("students_info.xlsx")
 df1 = df1[df1['login'].notna()]
-gr_f = df1['group_faculty'].unique()
-gr_o = df1['group_out'].unique()
+
+names = [df1['group_faculty'].unique(), np.sort(df1['group_out'].unique())]
 
 df2 = pd.read_html("results_ejudge.html")[0].rename(columns={'User': 'login'})
 
 df = pd.merge(df2, df1, on='login')
 del df1, df2
 
-# print(df)
+f_stat = df.groupby('group_faculty').mean()['Solved'].to_list()
+o_stat = df.groupby('group_out').mean()['Solved'].to_list()
 
-f_stat, o_stat = [], []
-for i in gr_f:
-    f_stat.append([i, df.loc[df['group_faculty'] == i, 'Solved'].sum() / df[df['group_faculty'] == i].shape[0]])
-
-for i in gr_o:
-    o_stat.append([i, df.loc[df['group_out'] == i, 'Solved'].sum() / df[df['group_out'] == i].shape[0]])
-
-o_stat.sort()
 data = [f_stat, o_stat]
-# print(data)
 
 fig, axes = plt.subplots(ncols=2, nrows=1, gridspec_kw={"wspace": 0.8}, figsize=[11, 8])
 
 for i in range(2):
-    axes[i].bar([i for i in range(7)], height=[data[i][k][1] for k in range(7)])
+    axes[i].bar([i for i in range(7)], height=[data[i][k] for k in range(7)])
     axes[i].set_xticks([i for i in range(7)])
-    axes[i].set_xticklabels((k[0] for k in data[i]))
+    axes[i].set_xticklabels((k for k in names[i]))
 
 axes[0].set_title("Фак. группы")
 axes[1].set_title("Инф. группы")
